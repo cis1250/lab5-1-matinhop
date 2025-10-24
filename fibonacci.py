d0f571b2a882971292f0b5d1151d6f74bf88c03a
Sayedmatin Hosseini <sayedmat@uoguelph.ca>
2025-10-24 11:41:58 -0400

    Fix comment syntax for TODO instruction

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # Fibonacci Sequence Exercise with functions
-# TODO: (Read detailed instructions in the Readme file)
+# TODO: (Read detailed instructions in the Readme file
